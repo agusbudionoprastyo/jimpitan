081302be19a14c0ec7e7937a89a5c9bb95aba9e8
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-09 22:41:50 +0700

    Update report.js

--- dashboard/js/report.js
+++ dashboard/js/report.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
-	// table initialize
-	var table = new DataTable('#example', {
-		searching: true, // Aktifkan pencarian
-		order: [[1, 'asc']], // Urutkan berdasarkan kolom kedua (indeks 1), urutan ascending
-		columnDefs: [
-			{ 
-				"orderable": false, 
-				"targets": [0] // Disable ordering for the third column (index 0)
-			},
-			{ 
-				"targets": 1,  // Kolom ke-2 (indeks mulai dari 0)
-				"className": "text-left" 
-			}
-		]
-	});
+	// // table initialize
+	// var table = new DataTable('#example', {
+	// 	searching: true, // Aktifkan pencarian
+	// 	order: [[1, 'asc']], // Urutkan berdasarkan kolom kedua (indeks 1), urutan ascending
+	// 	columnDefs: [
+	// 		{ 
+	// 			"orderable": false, 
+	// 			"targets": [0] // Disable ordering for the third column (index 0)
+	// 		},
+	// 		{ 
+	// 			"targets": 1,  // Kolom ke-2 (indeks mulai dari 0)
+	// 			"className": "text-left" 
+	// 		}
+	// 	]
+	// });
+
+    // Table initialize
+    var table = new DataTable('#example', {
+        searching: true, // Enable searching
+        order: [[1, 'asc']], // Sort by the second column (index 1), ascending order
+        columnDefs: [
+            { 
+                orderable: false, 
+                targets: [0] // Disable ordering for the first column (index 0)
+            },
+            { 
+                targets: 1,  // Second column (index starting from 0)
+                className: "text-left" 
+            }
+        ],
+        language: {
+            lengthMenu: "Display _MENU_ records per page",
+            zeroRecords: "No records found",
+            info: "Showing page _PAGE_ of _PAGES_",
+            infoEmpty: "No records available",
+            infoFiltered: "(filtered from _MAX_ total records)"
+        }
+    });
 
     // Event listener untuk "Enter" pada #search-input
     $('#search-input').keypress(function(event) {
