83e5621f7d34c8757901cdb1ff7aeb1750aa0a3c
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-09 16:58:19 +0700

    ..

--- dashboard/report.php
+++ dashboard/report.php
@@ -61,20 +61,20 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </li>
         </ul>
 
-        <!-- <ul class="side-menu">
-			<li>
+        <ul class="side-menu">
+			<!-- <li>
 				<a href="#">
 					<i class='bx bxs-cog' ></i>
 					<span class="text">Settings</span>
 				</a>
-			</li>
+			</li> -->
 			<li>
 				<a href="#" class="logout">
 					<i class='bx bxs-log-out-circle' ></i>
 					<span class="text">Logout</span>
 				</a>
 			</li>
-		</ul> -->
+		</ul>
     </section>
     <!-- SIDEBAR -->
 
