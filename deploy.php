a03002f4c0f1e013ce8e9c730ea2c6ae39806d80
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-09 13:47:45 +0700

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,11 +1,20 @@
 <?php
 $repository = 'https://github.com/agusbudionoprastyo/jimpitan.git';
-$targetDir = '/home/dafm5634/public_html/jimpitan'; // Path to your target directory
+$targetDir = '/home/dafm5634/public_html/jimpitan';
 
 // Change to the target directory
 chdir($targetDir);
 
 // Pull the latest changes
-exec("git pull $repository master");
+$output = [];
+$return_var = 0;
+
+exec("git pull $repository main 2>&1", $output, $return_var);
+
+if ($return_var !== 0) {
+    // Jika ada kesalahan, tampilkan output
+    echo "Error pulling repository: " . implode("\n", $output);
+} else {
+    echo "Repository updated successfully.";
+}
 ?>
-<!-- auto push event from github -->
