1f238d3d7ab0bd7efe4eea7565b08483102ccd77
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-10 15:00:54 +0700

    Update export.php

--- dashboard/export.php
+++ dashboard/export.php
@@ -10,17 +10,7 @@ $spreadsheet = new Spreadsheet();
 $activeWorksheet = $spreadsheet->getActiveSheet();
 $activeWorksheet->setCellValue('A3', 'No !');
 $activeWorksheet->setCellValue('B3', 'Nama KK !');
-
-// Start populating dates from column C (C3, D3, E3, ...)
-$startDate = new DateTime('2024-10-01');
-$endDate = new DateTime('2024-10-31');
-
-// Set column headers for dates
-$column = 3; // Column C is the 3rd column
-for ($date = $startDate; $date <= $endDate; $date->modify('+1 day')) {
-    $activeWorksheet->setCellValueByColumnAndRow($column, 3, $date->format('d/m/Y')); // Set date in row 3
-    $column++;
-}
+$activeWorksheet->setCellValue('C3', '01/10/2024 !');
 
 // Set the headers to prompt a download
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
