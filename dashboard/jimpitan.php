54c87aa8a0371cc20ba73dc6f00cca0fae8f52a9
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-13 03:23:29 +0700

    ..

---
+++ dashboard/jimpitan.php
@@ -0,0 +1,261 @@
+<?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['user'])) {
+    header('Location: ../login.php'); // Redirect to login page
+    exit;
+}
+
+// Check if user is admin
+if ($_SESSION['user']['role'] !== 'admin') {
+    header('Location: ../login.php'); // Redirect to unauthorized page
+    exit;
+}
+// Include the database connection
+include 'api/db.php';
+
+// Prepare the SQL statement to select only today's shift
+$stmt = $pdo->prepare("
+    SELECT master_kk.kk_name, report.* 
+    FROM report 
+    JOIN master_kk ON report.report_id = master_kk.code_id
+");
+
+// Execute the SQL statement
+$stmt->execute();
+
+// Fetch all results
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/2.0.8/css/dataTables.tailwindcss.css" rel="stylesheet">
+
+    <!-- Boxicons -->
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- sweetalert2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js"></script>
+    <title>Jimpitan</title>
+</head>
+<body>
+
+    <!-- SIDEBAR -->
+    <section id="sidebar">
+        <a href="#" class="brand">
+            <i class='bx bx-square-rounded'></i>
+            <span class="text">Jimpitan</span>
+        </a>
+        <ul class="side-menu top">
+            <li>
+                <a href="index.php">
+                    <i class='bx bxs-dashboard' ></i>
+                    <span class="text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+            <a href="kk.php">
+                    <i class='bx bxs-group' ></i>
+                    <span class="text">KK</span>
+                </a>
+            </li>
+            <li class="active">
+            <a href="#">
+                    <i class='bx bxs-report'></i>
+                    <span class="text">Report</span>
+                </a>
+            </li>
+            <li>
+            <a href="keuangan.php">
+                    <i class='bx bxs-report'></i>
+                    <span class="text">Keuangan</span>
+                </a>
+            </li>
+        </ul>
+
+        <ul class="side-menu">
+			<!-- <li>
+				<a href="#">
+					<i class='bx bxs-cog' ></i>
+					<span class="text">Settings</span>
+				</a>
+			</li> -->
+			<li>
+				<a href="logout.php" class="logout">
+					<i class='bx bxs-log-out-circle' ></i>
+					<span class="text">Logout</span>
+				</a>
+			</li>
+		</ul>
+    </section>
+    <!-- SIDEBAR -->
+
+    <!-- CONTENT -->
+    <section id="content">
+        <!-- NAVBAR -->
+        <nav>
+            <i class='bx bx-menu' ></i>
+            <form action="#">
+                <div class="form-input">
+                    <input type="search" id="search-input" placeholder="Search...">
+                    <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+					<!-- <button type="button" class="clear-btn"><i class='bx bx-reset' ></i></button> -->
+                </div>
+            </form>
+            <input type="checkbox" id="switch-mode" hidden>
+            <label for="switch-mode" class="switch-mode"></label>
+        </nav>
+        <!-- NAVBAR -->
+
+        <!-- MAIN -->
+        <main>
+            <div class="head-title">
+                <div class="left">
+                    <h1>Jimpitan - RT07 Salatiga</h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="#">Report</a>
+                        </li>
+                        <li><i class='bx bx-chevron-right' ></i></li>
+                        <li>
+                            <a class="active" href="index.php">Home</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="table-data">
+                <div class="order">
+                    <div class="head">
+                        <h3>Report</h3>
+                        <!-- <select id="month" name="month" class="custom-select">
+                            <?php for ($i = 1; $i <= 12; $i++): ?>
+                                <option value="<?= $i ?>" <?= ($i == date('n')) ? 'selected' : '' ?>>
+                                    <?= date('F', mktime(0, 0, 0, $i, 1)) ?>
+                                </option>
+                            <?php endfor; ?>
+                        </select>
+                        <select id="year" name="year" class="custom-select">
+                            <?php for ($y = date('Y'); $y >= 2000; $y--): ?>
+                                <option value="<?= $y ?>" <?= ($y == date('Y')) ? 'selected' : '' ?>><?= $y ?></option>
+                            <?php endfor; ?>
+                        </select> -->
+                        <input type="text" id="monthPicker" name="month-year" class="custom-select" placeholder="Pilih Bulan & Tahun">
+                        
+                            <button type="button" id="reportBtn" class="btn-download">
+                                <i class='bx bxs-file-export'></i> Unduh
+                            </button>
+                    </div>
+                    <table id="example" class="display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Nama KK</th>
+                                <th>Code</th>
+                                <th>Jimpitan Tanggal</th>
+                                <th>Nominal</th>
+                                <th>Input By</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                if ($data) {
+                                    foreach ($data as $row): ?>
+                                        <tr>
+                                            <td><?php echo htmlspecialchars($row["kk_name"]); ?></td>
+                                            <td><?php echo htmlspecialchars($row["report_id"]); ?></td>
+                                            <td><?php echo htmlspecialchars($row["jimpitan_date"]); ?></td>
+                                            <td><?php echo htmlspecialchars($row["nominal"]); ?></td>
+                                            <td><?php echo htmlspecialchars($row["collector"]); ?></td>
+                                        </tr>
+                                    <?php endforeach; 
+                                } else {
+                                    echo '<tr><td colspan="5" class="px-6 py-4 text-center">No data available</td></tr>';
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+        <!-- MAIN -->
+    </section>
+    <!-- CONTENT --> 
+    <script src="js/monthSelectPlugin.js"></script>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.tailwindcss.js"></script>
+
+    <script src="js/script.js"></script>
+    <script src="js/report.js"></script>
+    <script src="js/export.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.2.1/exceljs.min.js"></script>
+
+
+    <script>
+        flatpickr("#monthPicker", {
+            plugins: [
+                new monthSelectPlugin({
+                    shorthand: true, // Gunakan nama bulan singkat (Jan, Feb, Mar, dll.)
+                    dateFormat: "F Y", // Format untuk nilai yang dikembalikan
+                    altFormat: "F Y", // Format untuk tampilan input
+                })
+            ],
+            onChange: function(selectedDates, dateStr, instance) {
+                console.log("Bulan dan tahun yang dipilih:", dateStr);
+            }
+        });
+
+        const searchButton = document.querySelector('#content nav form .form-input button');
+        const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+        const searchForm = document.querySelector('#content nav form');
+
+        searchButton.addEventListener('click', function (e) {
+            if(window.innerWidth < 576) {
+                e.preventDefault();
+                searchForm.classList.toggle('show');
+                if(searchForm.classList.contains('show')) {
+                    searchButtonIcon.classList.replace('bx-search', 'bx-x');
+                } else {
+                    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+                }
+            }
+        })
+
+        if(window.innerWidth < 768) {
+            sidebar.classList.add('hide');
+        } else if(window.innerWidth > 576) {
+            searchButtonIcon.classList.replace('bx-x', 'bx-search');
+            searchForm.classList.remove('show');
+        }
+
+        window.addEventListener('resize', function () {
+            if(this.innerWidth > 576) {
+                searchButtonIcon.classList.replace('bx-x', 'bx-search');
+                searchForm.classList.remove('show');
+            }
+        })
+    </script>
+</body>
+</html>
--- dashboard/report.php
+++ dashboard/report.php
@@ -15,12 +15,18 @@ if ($_SESSION['user']['role'] !== 'admin') {
 // Include the database connection
 include 'api/db.php';
 
+// Get the current day of the week (e.g., "Sunday", "Monday", etc.)
+$currentDay = date('l'); // 'l' gives full textual representation of the day
+
 // Prepare the SQL statement to select only today's shift
 $stmt = $pdo->prepare("
-    SELECT master_kk.kk_name, report.* 
-    FROM report 
-    JOIN master_kk ON report.report_id = master_kk.code_id
+    SELECT user_name, shift 
+    FROM users 
+    WHERE shift = :currentDay
 ");
+
+// Bind the parameter
+$stmt->bindParam(':currentDay', $currentDay);
 
 // Execute the SQL statement
 $stmt->execute();
@@ -30,17 +36,13 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Tailwind CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-
-    <!-- DataTables CSS -->
-    <link href="https://cdn.datatables.net/2.0.8/css/dataTables.tailwindcss.css" rel="stylesheet">
 
     <!-- Boxicons -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
@@ -48,16 +50,9 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
 
-    <!-- sweetalert2 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js"></script>
-    <title>Jimpitan</title>
+    <title>Jimpitan - RT07 Salatiga</title>
 </head>
 <body>
-
     <!-- SIDEBAR -->
     <section id="sidebar">
         <a href="#" class="brand">
@@ -65,8 +60,8 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <span class="text">Jimpitan</span>
         </a>
         <ul class="side-menu top">
-            <li>
-                <a href="index.php">
+            <li class="active">
+                <a href="#">
                     <i class='bx bxs-dashboard' ></i>
                     <span class="text">Dashboard</span>
                 </a>
@@ -77,10 +72,16 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <span class="text">KK</span>
                 </a>
             </li>
-            <li class="active">
-            <a href="#">
+            <li>
+            <a href="report.php">
                     <i class='bx bxs-report'></i>
                     <span class="text">Report</span>
+                </a>
+            </li>
+            <li>
+            <a href="Jimpitan.php">
+                    <i class='bx bxs-report'></i>
+                    <span class="text">Jimpitan</span>
                 </a>
             </li>
             <li>
@@ -90,14 +91,13 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </a>
             </li>
         </ul>
-
         <ul class="side-menu">
-			<!-- <li>
-				<a href="#">
+			<li>
+				<a href="setting.php">
 					<i class='bx bxs-cog' ></i>
 					<span class="text">Settings</span>
 				</a>
-			</li> -->
+			</li>
 			<li>
 				<a href="logout.php" class="logout">
 					<i class='bx bxs-log-out-circle' ></i>
@@ -115,9 +115,8 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <i class='bx bx-menu' ></i>
             <form action="#">
                 <div class="form-input">
-                    <input type="search" id="search-input" placeholder="Search...">
-                    <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
-					<!-- <button type="button" class="clear-btn"><i class='bx bx-reset' ></i></button> -->
+                    <!-- <input type="search" id="search-input" placeholder="Search...">
+                    <button type="submit" class="search-btn"><i class='bx bx-search' ></i></button> -->
                 </div>
             </form>
             <input type="checkbox" id="switch-mode" hidden>
@@ -129,133 +128,100 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <main>
             <div class="head-title">
                 <div class="left">
-                    <h1>Jimpitan - RT07 Salatiga</h1>
+                <h1>Jimpitan - RT07 Salatiga</h1>
                     <ul class="breadcrumb">
                         <li>
-                            <a href="#">Report</a>
+                            <a href="#">Dashboard</a>
                         </li>
                         <li><i class='bx bx-chevron-right' ></i></li>
                         <li>
-                            <a class="active" href="index.php">Home</a>
+                            <a class="active" href="#">Home</a>
                         </li>
                     </ul>
                 </div>
             </div>
+            
+            <ul class="box-info">
+                <li>
+                    <i class='bx bxs-group bx-lg' ></i>
+                    <span class="text">
+                        <h3 id="totalPeserta">0</h3>
+                        <p>Total KK</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-badge-check bx-lg' ></i>
+                    <span class="text">
+                        <h3 id="totalCheck">0</h3>
+                        <p>Checked</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-info-circle bx-lg'></i>
+                    <span class="text">
+                        <h3 id="totalUncheck">0</h3>
+                        <p>Unchecked</p>
+                    </span>
+                </li>
+            </ul>
+
 
             <div class="table-data">
-                <div class="order">
-                    <div class="head">
-                        <h3>Report</h3>
-                        <!-- <select id="month" name="month" class="custom-select">
-                            <?php for ($i = 1; $i <= 12; $i++): ?>
-                                <option value="<?= $i ?>" <?= ($i == date('n')) ? 'selected' : '' ?>>
-                                    <?= date('F', mktime(0, 0, 0, $i, 1)) ?>
-                                </option>
-                            <?php endfor; ?>
-                        </select>
-                        <select id="year" name="year" class="custom-select">
-                            <?php for ($y = date('Y'); $y >= 2000; $y--): ?>
-                                <option value="<?= $y ?>" <?= ($y == date('Y')) ? 'selected' : '' ?>><?= $y ?></option>
-                            <?php endfor; ?>
-                        </select> -->
-                        <input type="text" id="monthPicker" name="month-year" class="custom-select" placeholder="Pilih Bulan & Tahun">
-                        
-                            <button type="button" id="reportBtn" class="btn-download">
-                                <i class='bx bxs-file-export'></i> Unduh
-                            </button>
-                    </div>
-                    <table id="example" class="display" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Nama KK</th>
-                                <th>Code</th>
-                                <th>Jimpitan Tanggal</th>
-                                <th>Nominal</th>
-                                <th>Input By</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                if ($data) {
-                                    foreach ($data as $row): ?>
-                                        <tr>
-                                            <td><?php echo htmlspecialchars($row["kk_name"]); ?></td>
-                                            <td><?php echo htmlspecialchars($row["report_id"]); ?></td>
-                                            <td><?php echo htmlspecialchars($row["jimpitan_date"]); ?></td>
-                                            <td><?php echo htmlspecialchars($row["nominal"]); ?></td>
-                                            <td><?php echo htmlspecialchars($row["collector"]); ?></td>
-                                        </tr>
-                                    <?php endforeach; 
-                                } else {
-                                    echo '<tr><td colspan="5" class="px-6 py-4 text-center">No data available</td></tr>';
-                                }
-                            ?>
-                        </tbody>
-                    </table>
+				<div class="order">
+					<div class="head">
+                    <h3>Jaga Malam</h3>
+					</div>
+					<table id="checkin-table">
+						<thead>
+							<tr>
+								<th>NAMA</th>
+								<th>SHIFT</th>
+							</tr>
+						</thead>
+						<tbody>
+                        <?php
+                            if ($data) {
+                                foreach ($data as $row): ?>
+                                    <tr>
+                                        <td><?php echo htmlspecialchars($row["user_name"]); ?></td>
+                                        <td><?php echo htmlspecialchars($row["shift"]); ?></td>
+                                    </tr>
+                                <?php endforeach; 
+                            } else {
+                                echo '<tr><td colspan="3">No data available</td></tr>';
+                            }
+                        ?>
+						</tbody>
+					</table>
                 </div>
-            </div>
-        </main>
-        <!-- MAIN -->
-    </section>
-    <!-- CONTENT --> 
-    <script src="js/monthSelectPlugin.js"></script>
+
+                    <!-- <div class="todo">
+                        <div class="head">
+                            <h3>Jaga Malam</h3>
+                        </div>
+                                <ul id="medal-list" class="todo-list">
+                            <li class="first">
+                            </li>
+                        <li class="second">
+                        </li>
+                    <li class="third">
+                    </li>
+                        <li class="fourth">
+                        </li>
+                            <li class="fifth">
+                            </li>
+                            <li class="sixth">
+                            </li>
+                                </ul>
+                            </div> -->
+                        </div>
+                    </main>
+            <!-- MAIN -->
+        </section>
+    
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- DataTables JS -->
-    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
-    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.tailwindcss.js"></script>
-
     <script src="js/script.js"></script>
-    <script src="js/report.js"></script>
-    <script src="js/export.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.2.1/exceljs.min.js"></script>
-
-
-    <script>
-        flatpickr("#monthPicker", {
-            plugins: [
-                new monthSelectPlugin({
-                    shorthand: true, // Gunakan nama bulan singkat (Jan, Feb, Mar, dll.)
-                    dateFormat: "F Y", // Format untuk nilai yang dikembalikan
-                    altFormat: "F Y", // Format untuk tampilan input
-                })
-            ],
-            onChange: function(selectedDates, dateStr, instance) {
-                console.log("Bulan dan tahun yang dipilih:", dateStr);
-            }
-        });
-
-        const searchButton = document.querySelector('#content nav form .form-input button');
-        const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-        const searchForm = document.querySelector('#content nav form');
-
-        searchButton.addEventListener('click', function (e) {
-            if(window.innerWidth < 576) {
-                e.preventDefault();
-                searchForm.classList.toggle('show');
-                if(searchForm.classList.contains('show')) {
-                    searchButtonIcon.classList.replace('bx-search', 'bx-x');
-                } else {
-                    searchButtonIcon.classList.replace('bx-x', 'bx-search');
-                }
-            }
-        })
-
-        if(window.innerWidth < 768) {
-            sidebar.classList.add('hide');
-        } else if(window.innerWidth > 576) {
-            searchButtonIcon.classList.replace('bx-x', 'bx-search');
-            searchForm.classList.remove('show');
-        }
-
-        window.addEventListener('resize', function () {
-            if(this.innerWidth > 576) {
-                searchButtonIcon.classList.replace('bx-x', 'bx-search');
-                searchForm.classList.remove('show');
-            }
-        })
-    </script>
 </body>
 </html>
