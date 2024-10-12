937d27aeea4f881c9e786abd16219842f9ede75e
abiydoni <abiydoni@gmail.com>
2024-10-13 01:46:40 +0700

    Update report.php

--- dashboard/report.php
+++ dashboard/report.php
@@ -83,13 +83,13 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <span class="text">Report</span>
                 </a>
             </li>
-        </ul>x
-        <li class="active">
-            <a href="keuangan.php">
+            <li class="active">
+                <a href="keuangan.php">
                     <i class='bx bxs-report'></i>
                     <span class="text">Keuangan</span>
                 </a>
             </li>
+        </ul>
 
         <ul class="side-menu">
 			<!-- <li>
