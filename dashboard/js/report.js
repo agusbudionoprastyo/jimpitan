c70cfe6318ab0ac30c4f6fcf8ed4fdbdc7910926
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-13 12:42:09 +0700

    Update report.js

--- dashboard/js/report.js
+++ dashboard/js/report.js
@@ -5,6 +5,29 @@ $(document).ready(function() {
         lengthMenu: [10, 25, 50, 100], // Options for the dropdown
         searching: true, // Enable searching
         order: [[2, 'desc']], // Sort by the third column (index 2), descanding order
+        columnDefs: [
+			{ 
+				"targets": 0,  // Kolom ke-1 (indeks mulai dari 0)
+				"className": "text-left" 
+			},
+			{ 
+				"targets": 1,  // Kolom ke-2 (indeks mulai dari 0)
+				"className": "text-center" 
+			}
+            ,
+			{ 
+				"targets": 2,  // Kolom ke-3 (indeks mulai dari 0)
+				"className": "text-center" 
+			},
+			{ 
+				"targets": 3,  // Kolom ke-4 (indeks mulai dari 0)
+				"className": "text-center" 
+			},
+			{ 
+				"targets": 4,  // Kolom ke-5 (indeks mulai dari 0)
+				"className": "text-center" 
+			}
+		],
         language: {
             lengthMenu: "_MENU_ Entri per halaman",
             zeroRecords: "No records found",
