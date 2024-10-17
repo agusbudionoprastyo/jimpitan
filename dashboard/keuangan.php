a763d377cc77d2faf73ce34ed2adccf70a9e9e3c
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-10-17 10:43:04 +0700

    Revert "Update keuangan.php"
    This reverts commit 501d7464a851b96f63cb4c91b61196893a4eb5fa.

--- dashboard/keuangan.php
+++ dashboard/keuangan.php
@@ -152,7 +152,7 @@ if (isset($_POST['tanggal'])) {
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="modalLabel">Form Tambah Transaksi</h5>
+                            <h5 class="modal-title" id="modalLabel">Form Tambah Data</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
