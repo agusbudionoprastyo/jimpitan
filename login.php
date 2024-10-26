2dbd5a67c48c0b66ab9f11a5f8208609c9cf9c7f
abiydoni <abiydoni@gmail.com>
2024-10-26 18:27:20 +0700

    Update login.php

--- login.php
+++ login.php
@@ -89,7 +89,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="error-message" style="color: red; font-size: 12px;"><?php echo htmlspecialchars($error); ?></div>
         <?php endif; ?>
     </div>
-    <p style="color:grey font-size: 80%;">@2024 copyright | by doniabiy</p>
+    <p style="color:grey">@2024 copyright | by doniabiy</p>
 </div>
 </form>
     <!-- <script>
